f8c1f2dcf9dafe90fdd80a1849f7aea7995d7c70
Gerardo <ferrari_gerardo@live.com.mx>
2018-09-10 16:41:19 -0500

    Ultima version

---
+++ QRCodeBBVA/QRCodeBBVA/LogViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LogViewController.swift
+//  QRCodeBBVA
+//
+//  Created by Gerardo on 08/09/18.
+//  Copyright © 2018 Gerardo. All rights reserved.
+//
+
+import UIKit
+
+class LogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
